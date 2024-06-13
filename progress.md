7ee87b4aeee216f53ecd420f31e7131c956e47cf
Michael Zonneveld <michael.zonneveld@live.nl>
2024-06-13 10:33:05 +0200

    wip

---
+++ progress.md
@@ -0,0 +1,19 @@
+### 7 juni 
+
+misschien moet ik de grid wel laten bestaan uit een 2d array, maar als ik ga zoeken voor een sequence een andere data structuur gebruiken zoals set of map. 
+
+maak er een 2d grid van 
+als er een wijziging in de grid komt, maak dan een subcollectie van arrays - bestaande uit vijf objecten, inclusief locatie van elke cellwaarde. Deze subcollectie moet alleen cellen bevatten die kans maken om geleegd te worden. Dus, het zullen altijd vier aangrenzende buren zijn van de cellen die opgewaardeerd worden plus de opgewaarde cel. 
+
+eerst focussen op collecties maken voor het controleren voor reeksen van links naar rechts 
+dan van boven naar onder 
+als bonus kan ik later ook nog doen van rechts naar links en van onder naar boven 
+extra bonus is diagonaal. 
+
+misschien kan voor elke reeks van vijf die je controleerd, hem ontoveren in een map, zodat je snel kan controleren of er wel een waarde is in de reeks die voorkomt in de fibonacci reeks, alvorens je waardes met elkaar gaat vergelijken. 
+
+je kan subSetof gebruiken voor de reeks die je checkt, die controleert of alle waardes onderdeel zijn van een set. alleen werkt dit niet voor firefox. mijn plan is om die voor nu even te gebruiken, en dan later een listener voor de browser toe te voegen die (https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent). 
+
+er zal altijd een orde gemaakt moeten worden naar keuze van de programmeur of je eerst van boven naar beneden of van beneden naar boven controleert
+
+in mijn oude spel, kon je een sequence van zes maken, bijvoorbeeld van 3, 5, 8, 13, 21, 34. Omdat ie hier 2x een sequence ziet, namelijk 3, 5, 8, 13, 21 en 5, 8, 13, 21, 34. Maar dat is gek, want dan lijkt het voor de gebruiker alsof je een sequence van zes maakt, terwijl in wijze hij 2 keer een sequence van 5 doet, omdat ie in mijn oude spel alles de hele tijd controleert. maar als je nou zegt hee ik controleer alleen de vier aangrenzende buren van een cell die wordt opgewaardeerd, dan heb je dat probleem niet. 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,192 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233 
+
+//  5, 8, 13, 21, 34, 55, 89
+
+
+// zo kan je er over heen loopen 
+// function logMapElements(value, key, map) {
+//   console.log(`[${key}] = ${value}`);
+// }
+
+// useEffect(() => {
+//   map.forEach(logMapElements);
+// }, [map]);
+// const cache = getNthFibonacci(13);
+// let exists = Object.values(cache).includes(89);
+
+
+const size = 10;
+
+const generateMap = (size) => {
+  const map = new Map();
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const key = `${i}:${j}`
+      map.set(key, null)
+    }
+  }
+  return map;
+};
+
+const getNthFibonacci = (n, cache = { 1: 0, 2: 1 }) => {
+  if (n in cache) {
+    return cache;
+  } else {
+    cache[n] = getNthFibonacci(n - 1, cache)[n - 1] + getNthFibonacci(n - 2, cache)[n - 2];
+    return cache;
+  }
+}
+
+
 function App() {
+  const [map, setMap] = useState(generateMap(size));
+  const [clickedRow, setClickedRow] = useState(null);
+  const [clickedCol, setClickedCol] = useState(null);
+  const cache = getNthFibonacci(30);
+  const fibs = Object.values(cache);
+  const fibSet = new Set(fibs);
+
+
+
+  const checkSequences = () => {
+
+    // eerst verticaal
+    const seq = [];
+
+    for (let i = 4; i >= 0; i--) {
+      // dit werkt voor nu alleen nog maar voor eerste kolom
+      let value = map.get(`${clickedRow - i}:${0}`)
+
+      // value is eerst null of undefined, maar deze check kan wel weg als die useEffect en de trigger voor deze functie beter is. 
+      if (value !== null || value !== undefined) {
+        seq.push(value);
+      }
+    };
+
+    // console.log('seq', seq);
+
+    const seqSet = new Set(seq);
+
+    // console.log('seqSet', seqSet)
+
+    // het is alleen true als ze er allemaal onderdeel van zijn en als de set een grote heeft van 5, elk element binnen een set moet uniek zijn.
+    const fibInSeq = seqSet.isSubsetOf(fibSet) && seqSet.size === 5;
+
+    if (fibInSeq) {
+      for (let i = 4; i >= 0; i--) {
+        map.delete(`${clickedRow - i}:${0}`);
+        console.log('updated map after fib seq ', map)
+      }
+
+
+      // hoe weet je welke coordinaten je moet hebben? 
+      // clearCells()
+    }
+  }
+
+  useEffect(() => {
+    checkSequences();
+  }, [map])
+
+  const incrementCells = (row, col, map) => {
+
+    // this can be set elsewhere, not a really nice spot, needed for check sequences functies because its lagging behind 
+    setClickedRow(row);
+    setClickedCol(col);
+
+    const newMap = new Map(map);
+
+    var key = `${row}:${col}`
+    var currentVal = map.get(key);
+
+    console.log('key', key)
+
+    // if (currentVal === NaN || currentVal === undefined) {
+      // newMap.set(key, 1)
+    // }
+
+    newMap.set(key, currentVal + 1);
+
+    for (let i = 0; i < size; i++) {
+      if (i !== col) {
+        const key = `${row}:${i}`;
+        var currentVal = map.get(key);
+        newMap.set(key, currentVal + 1);
+      }
+    };
+
+    for (let j = 0; j < size; j++) {
+      if (j !== row) {
+        const key = `${j}:${col}`;
+        var currentVal = map.get(key);
+        newMap.set(key, currentVal + 1);
+      }
+    };
+
+    setMap(newMap)
+  }
+
+  const updateMap = (row, col, map) => {
+    incrementCells(row, col, map);
+  }
+
+  const rows = Array.from(Array(size).keys());
+  const columns = Array.from(Array(size).keys());
+  const subarr = [1, 1, 2, 3, 5];
+  const subset = new Set(subarr);
+  const fibset = new Set([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]);
+
+  // wanneer chrome: 
+  const processSequenceChrome = subset.isSubsetOf(fibset);
+  // console.log('processSequenceChrome', processSequenceChrome);
+
+  // wanneer firefox
+  var count = 0;
+  var processSequenceFirefox = false;
+
+  for (let i = 0; i < 5; i++) {
+    if (fibset.has(subarr[i])) {
+      count++
+    }
+    if (count === 4) {
+      processSequenceFirefox = true;
+    }
+  }
+
+  // console.log('processSequenceFirefox', processSequenceFirefox);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th className='invisible'></th>
+            {columns.map((col) => (
+              <th key={col}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr key={row}>
+              <td className='first'>{row}</td>
+              {columns.map((col) => {
+                return (
+                  <td key={col} onClick={() => incrementCells(row, col, map)}>{map.get(`${row}:${col}`) ? map.get(`${row}:${col}`) : '' }</td>
+                )
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* <button onClick={() => geef()}> harry? </button> */}
+    </>
+  )
 }
 
 export default App;
