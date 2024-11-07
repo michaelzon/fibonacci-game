b4d99501731eefd02a2b2b8e52204eaaab89088d
Michael Zonneveld <michael.zonneveld@live.nl>
2024-11-07 01:01:43 +0100

    objwip

--- src/App.js
+++ src/App.js
@@ -2,40 +2,101 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import {isFibonacci} from "./helpers/isFibonacci";
 
-const size = 10;
+const fibos = [0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    21,
+    34,
+    55,
+    89,
+    144,
+    233,
+    377,
+    610,
+    987,
+    1597,
+    2584,
+    4181]
+
+const listOfFibs = [{"6:0": 6}, {"5:0": 5}, {"6:0": 6}]
+
+
+const size = 12;
 
 function App() {
     const [map, setMap] = useState(new Map());
     const [clickedRow, setClickedRow] = useState(null);
     const [clickedCol, setClickedCol] = useState(null);
-    const seqLength = 3;
+    const sequenceLength = 5;
     let fibonacciCount = 0;
-    
+
 
     useEffect(() => {
-        console.log('clickedCol', clickedCol)
-        console.log('clickedRow', clickedRow)
-        console.log('map', map)
-        checkMapForFibonacciNumbers()
+        // console.log('clickedCol', clickedCol)
+        // console.log('clickedRow', clickedRow)
+        collectFibs()
+        searchFibNeighboursHorizontally()
     }, [map])
 
 
-    const cellsToCheck = {}
+    const fibsToCheck = {}
+    const fibsToCheckMap = new Map();
 
-    const checkMapForFibonacciNumbers = () => {
+    const collectFibs = () => {
         map.forEach((value, key) => {
             if (isFibonacci(value)) {
-                fibonacciCount++
-                cellsToCheck[key] = value;
-                // console.log(`${value} (at key '${key}') is a Fibonacci number.`);
-            } else {
-                // console.log(`${value} (at key '${key}') is NOT a Fibonacci number.`);
+                fibsToCheck[key] = value;
+                fibsToCheckMap.set(key, value);
             }
         })
-        // console.log("Total Fibonacci numbers found:", fibonacciCount);
     };
 
-    console.log('cells to check', cellsToCheck);
+    // console.log(fibsToCheck)
+    // console.log(fibsToCheckMap)
+
+    const sequences = [];
+
+    const fiboInRowsToBeChecked = [];
+    console.log('fiboInRowsToBeChecked', fiboInRowsToBeChecked)
+
+    // ze zijn hier eerst voor row en dan voor column gesorteerd 0:0 0:5 0:6
+    const searchFibNeighboursHorizontally = () => {
+        for (let col = 0; col < size; col++) {
+            const fibo = fibsToCheck[`0:${col}`];
+
+            fiboInRowsToBeChecked.push(fibo)
+        }
+
+            // sequences.push(keysSorted.slice(i, i + sequenceLength))
+            // for (let j = 0; j < sequenceLength; j++) {
+            //     const key = keysSorted[i + j];
+            // const rowIndex = parseInt(Array.from(key)[0]);
+            // const colIndex = parseInt(key.slice(-1));
+            // console.log('key 1', key);
+            // console.log('key 2', rowIndex, colIndex);
+            // console.log('value', fibsToCheck[keysSorted[i + j]]);
+            // sequence.push({key: keysSorted[i + j], value: fibsToCheck[keysSorted[i + j]]});
+        }
+        // sequences.push(sequence)
+        // }
+
+    // const rowIndex = parseInt(Array.from(key)[0]);
+    // const colIndex = parseInt(key.slice(-1));
+    // const rowIndex = Array.from(key)[0];
+    // const colIndex = key.slice(-1);
+    // console.log('rowIndex', rowIndex)
+    // console.log('colIndex', colIndex)
+    // console.log('key', key);
+    // console.log('value', value);
+    // const keys = Object.keys(fibsToCheck);
+    // console.log(keys);
+
+    console.log('sequences', sequences)
 
     const incrementCells = (row, col, map) => {
 
@@ -44,23 +105,40 @@ function App() {
 
         const newMap = new Map(map);
 
-        for (let y = 0; y < size; y++) {
-            const key = `${row}:${y}`;
+        for (let x = 0; x < size; x++) {
+            const key = `${row}:${x}`;
             const colVal = map.get(key);
             newMap.set(key, (colVal === undefined || isNaN(colVal)) ? 1 : colVal + 1)
         }
 
-        for (let x = 0; x < size; x++) {
-            const key = `${x}:${col}`;
+        for (let y = 0; y < size; y++) {
+            const key = `${y}:${col}`;
             const rowVal = map.get(key);
             newMap.set(key, (rowVal === undefined || isNaN(rowVal)) ? 1 : rowVal + 1)
         }
-
         setMap(newMap)
     }
 
     const rows = Array.from(Array(size).keys());
     const columns = Array.from(Array(size).keys());
+
+    function check([x, y]) {
+        return (x in coords) && (y in coords[x]);
+    }
+
+    var data = [[0, 1], [0, 2], [2, 3]];
+    var coords = new Map();
+
+    data.forEach(([x, y]) => {
+        console.log('x', x);
+        coords[x] = coords[x] || Object.create(null);
+        coords[x][y] = true;                          // or any other needed value
+    });
+
+    console.log(check([0, 2]));
+    console.log(check([0, 0]));
+    console.log('coords', coords);
+    console.log('map', map)
 
     return (
         <>
